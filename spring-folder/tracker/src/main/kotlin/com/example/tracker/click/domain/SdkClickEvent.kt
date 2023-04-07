3f0ce53696922a279992717789d00080a1065cf2
cherry <promessa93@gmail.com>
2023-04-07 11:11:55 +0900

    style[tracker]: DynamoDB aws sdk kotlin은 Domain 빈 필요 없음

--- spring-folder/tracker/src/main/kotlin/com/example/tracker/click/domain/SdkClickEvent.kt
+++ spring-folder/tracker/src/main/kotlin/com/example/tracker/click/domain/SdkClickEvent.kt
@@ -1,15 +1,8 @@
 package com.example.tracker.click.domain
 
-import com.example.tracker.common.converter.SdkLocalDateConverter
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*
 import java.time.LocalDateTime
 
-@DynamoDbBean
 data class SdkClickEvent(
-    @get:DynamoDbPartitionKey
     var id: Long? = null,
-
-    @get:DynamoDbSortKey
-    @get:DynamoDbConvertedBy(SdkLocalDateConverter::class)
     val regDtm: LocalDateTime? = null
 )
