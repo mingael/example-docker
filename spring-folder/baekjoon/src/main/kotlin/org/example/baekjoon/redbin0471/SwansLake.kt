631fd1ea766fd5d7617a6673f0f3ef5bd406b0d4
mingael <promessa93@gmail.com>
2023-08-28 21:35:51 +0900

    refactor: baekjoon example - SwansLake ing

--- spring-folder/baekjoon/src/main/kotlin/org/example/baekjoon/redbin0471/SwansLake.kt
+++ spring-folder/baekjoon/src/main/kotlin/org/example/baekjoon/redbin0471/SwansLake.kt
@@ -1,7 +1,12 @@
 package org.example.baekjoon.redbin0471
 
 import java.util.*
+import kotlin.collections.ArrayDeque
 
+/**
+ * 백조의 호수
+ * - 너비 우선 탐색(BFS, Breadth-First Search)과 큐(Queue)를 활용한 문제
+ */
 class SwansLake {
 
     companion object {
@@ -10,10 +15,12 @@ class SwansLake {
         private const val SWAN = 'L'
     }
 
-    private var row: Int = 0
-    private var col: Int = 0
+    private var R = 0
+    private var C = 0
 
     private lateinit var lake: Array<Array<Char>>
+    private var waterQueue = ArrayDeque<Pair<Int, Int>>()   // 물 영역 (물로 변한 빙판)
+    private var swanQueue = ArrayDeque<Pair<Int, Int>>()    // 백조 위치
 
     // 빙판 녹는 거리
     private val meltingDistance = arrayOf(Pair(-1, 0), Pair(0, -1), Pair(1, 0), Pair(0, 1))
@@ -26,7 +33,7 @@ class SwansLake {
 
 
         (1..3).forEach { day ->
-            meltingTheLake()
+            meltIce()
             println("후 $day")
             lake.forEach { arr -> arr.forEach { print(it) }; println() }
         }
@@ -38,14 +45,20 @@ class SwansLake {
     private fun init() {
         System.`in`.bufferedReader().run {
             StringTokenizer(readln()).run {
-                row = nextToken().toInt()
-                col = nextToken().toInt()
+                R = nextToken().toInt()
+                C = nextToken().toInt()
             }
 
             // 호수 초기 상태
-            lake = Array(row) { _ ->
+            lake = Array(R) { r ->
                 val str = StringTokenizer(readln()).nextToken()
-                Array(col) { str[it] }
+                Array(C) { c ->
+                    // 빙판을 제외한 모든 지점을 물 큐에 넣기
+                    if (str[c] != ICE_SHEET) waterQueue.add(Pair(r, c))
+                    // 백조 위치
+                    if (str[c] == SWAN) swanQueue.add(Pair(r, c))
+                    str[c]
+                }
             }
         }
     }
@@ -56,27 +69,37 @@ class SwansLake {
 
     /**
      * 빙판 녹이기
+     * - 인접한 방향(대각선 제외)이 빙판이면 해당 지점을 녹이고 녹인물 큐(meltQueue)에 넣는다. (BFS 수행)
+     * - 녹인물 큐(meltQueue)를 물 큐(waterQueue)에 갱신한다.
+     * - 함수가 실행될 때마다 이전에 탐색을 중지한 지점(빙판이 녹은 물)에서부터 BFS를 수행하게 된다.
+     * - 기존 물은 탐색하고나면 다시 방문하지 않기 때문에 탐색 횟수를 줄일 수 있다. (중복 탐색 제거)
      */
-    private fun meltingTheLake() {
-        val meltLake = Array(row) { Array(col) { ICE_SHEET } }
-        lake.forEachIndexed { r, rowCol ->
-            rowCol.forEachIndexed colReturn@{ c, value ->
-                if (value == ICE_SHEET) return@colReturn
+    private fun meltIce() {
+        // 녹여서 물이된 빙판
+        val meltQueue = ArrayDeque<Pair<Int, Int>>()
 
-                meltLake[r][c] = value
+        // 기존 물 영역
+        while (waterQueue.isNotEmpty()) {
+            val currNode = waterQueue.removeFirst()
+            val row = currNode.first
+            val col = currNode.second
 
-                if (value == WATER) {
-                    meltingDistance.forEach move@{
-                        val moveRow = r + it.first
-                        val moveCol = c + it.second
-                        if (moveRow < 0 || moveCol < 0) return@move
-                        if (moveRow >= row || moveCol >= col) return@move
-                        if (lake[moveRow][moveCol] == ICE_SHEET) meltLake[moveRow][moveCol] = WATER
-                    }
-                }
+            // 물과 인접한 빙판 녹이기
+            meltingDistance.forEach move@{
+                val moveRow = row + it.first
+                val moveCol = col + it.second
+
+                if (moveRow < 0 || moveCol < 0) return@move
+                if (moveRow >= R || moveCol >= C) return@move
+                if (lake[moveRow][moveCol] != ICE_SHEET) return@move
+
+                lake[moveRow][moveCol] = WATER
+                meltQueue.add(Pair(moveRow, moveCol))
             }
         }
-        lake = meltLake
+
+        // 녹여서 새로 생성된 물 지점
+        waterQueue = meltQueue
     }
 
 }
