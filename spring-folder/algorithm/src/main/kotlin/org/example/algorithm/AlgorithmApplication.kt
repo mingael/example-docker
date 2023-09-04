cce7433b54af0f5ef1f386ab6ed7072631e59129
mingael <promessa93@gmail.com>
2023-09-04 11:44:14 +0900

    rename: algorithm

---
+++ spring-folder/algorithm/src/main/kotlin/org/example/algorithm/AlgorithmApplication.kt
@@ -0,0 +1,16 @@
+package org.example.algorithm
+
+import org.example.algorithm.programmers.introduction.Parallel
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AlgorithmApplication
+
+fun main() {
+    Parallel().case1()
+    Parallel().case2()
+    Parallel().case3()
+    Parallel().case4()
+    Parallel().case5()
+    Parallel().case6()
+}
